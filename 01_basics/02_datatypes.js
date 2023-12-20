f34b6bec7a1cf304940f0ba9fe0addd81653fbfd
Hafiz Muneeb <124581370+HafizMuneeb@users.noreply.github.com>
2023-12-20 16:15:56 +0000

    data-types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+//  alert("Muneeb") we are using nodejs not browswer
+
+let name = "Muneeb" //its a string data type
+
+let age = 34 //its  a number data types
+
+let isLoggedin = true //boolean data type boolean data type only two values true or false
+
+let state;
+
+let fatherName = null;
+
+// null stand alone value in javascript null is a representation of empty value
+// undefined
+// symbol to find uniquness
+
+
+console.log(typeof "muneeb");
+console.log(typeof undefined);
+
+// non-primitive data type
