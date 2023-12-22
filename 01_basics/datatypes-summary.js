578c80684d493514c12d388fa216bc7cfe20a582
Hafiz Muneeb <124581370+HafizMuneeb@users.noreply.github.com>
2023-12-22 13:34:53 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,24 @@
+// *********** Primitive Data Types ************
+// Seven Categories
+
+ // String
+ // Number
+ // Boolean 
+ // Null
+ // undefined
+ // symbol 
+ // Bigint
+
+ const number = Symbol("123")
+ const anotherNumber = Symbol("123")
+
+console.table([number, anotherNumber]);
+
+console.log(number === anotherNumber);
+
+ // *********** No Primitive (Reference Type) Data Type *************
+
+ // Arrays
+ // Objects
+ // Functions
+
