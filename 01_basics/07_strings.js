3e8ba045bc881d56d74477359d8697e984d1b084
Hafiz Muneeb <124581370+HafizMuneeb@users.noreply.github.com>
2023-12-22 14:14:58 +0000

    strings in js

---
+++ 01_basics/07_strings.js
@@ -0,0 +1,14 @@
+const name = "Muneeb";
+
+const repoCount = 30;
+
+// console.log(name + repoCount);
+
+console.log(`Heelo my Name is ${name} and my repo Count is ${repoCount}`);
+
+
+const newName = new String("hafiz")
+
+
+const val = newName.italics()
+console.log(val);
