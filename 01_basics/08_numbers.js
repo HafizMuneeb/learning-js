765acf3e57ef90490584d725cedcafab2e1fb051
Hafiz Muneeb <124581370+HafizMuneeb@users.noreply.github.com>
2024-01-02 13:00:34 +0000

    numbers in javascript

---
+++ 01_basics/08_numbers.js
@@ -0,0 +1,22 @@
+const score = 400;
+
+// console.log(score.toFixed(3));
+
+const num = new Number(300)
+
+const num1 = num.toString();
+
+// console.log(num1.charAt(1));
+// console.log(num1);
+
+const hundreds = 100000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//learn number in detail
+
+
+
+// +++++++++++++++++ Maths +++++++++++++++++
+
+console.log(Math);
