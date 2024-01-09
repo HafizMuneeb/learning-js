b5f32bffc5cc16510d301dd3a66cf654a515e09c
Hafiz Muneeb <124581370+HafizMuneeb@users.noreply.github.com>
2024-01-09 13:15:48 +0000

    array in js

---
+++ arrays/Array_in_js.js
@@ -0,0 +1,21 @@
+// ways of declaring arrays:
+
+const myArr = [0, 1, 2, 3, 4]
+
+const myName = ["Hafiz", "Muneeb"]
+
+const myArr2 = new Array(1, 2, 3)
+
+myName.push("Afzal"); // .push() method add a value in an array
+
+myName.pop(); // .pop() method remove the last one element present is an array
+
+const newName = myName.join('-');
+
+// console.log(newName);
+
+const heros = ["hafiz", "muneeb"]
+
+const heros2 = ["Afzal", "haseeb"]
+heros.push(heros2);
+console.log(heros);
